5ec76ae7b303eeaaa056724c2c55cb3d9691205d
Mariusler <mariuler@stud.ntnu.no>
2017-10-10 16:03:02 +0200

    Algorithm working, need to create image
    Signed-off-by: Mariusler <mariuler@stud.ntnu.no>

---
+++ Assignment3/part1/part1.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strings"
+)
+
+const inf = int(math.MaxInt32 >> 1)
+
+type node struct {
+	y int
+	x int
+
+	g int
+	f int
+
+	weight int
+
+	open     bool // Is it in open er closed??
+	solution bool // Is this part of best path?
+	block    bool
+
+	parent *node
+	//kids   []node // In golang []*node and []node both create a slice, so they are the same. Slices are references to an array
+}
+
+type nodes []*node
+
+func (a nodes) Len() int {
+	return len(a)
+}
+
+func (a nodes) Less(i, j int) bool {
+	return a[i].f < a[j].f
+}
+
+func (a nodes) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func readBoard(board string) (*node, *node, [][]node) {
+	var startnodeX int
+	var startnodeY int
+	var endnodeX int
+	var endnodeY int
+	currentdir, _ := os.Getwd()
+	file, err := os.Open(currentdir + "/../" + "/boards/" + board)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	linecount := 0
+	board2 := make([][]node, 1)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		tempNodeRow := make([]node, 0)
+		line := scanner.Text()
+		for i, c := range line {
+			tempNode := node{x: i, y: linecount}
+			tempNode.g = inf
+			tempNode.f = inf
+			if c == '#' {
+				tempNode.block = true
+			} else if c == 'A' {
+				startnodeX = i
+				startnodeY = linecount
+			} else if c == 'B' {
+				endnodeX = i
+				endnodeY = linecount
+			}
+			tempNodeRow = append(tempNodeRow, tempNode)
+		}
+		if linecount == 0 {
+			board2[0] = tempNodeRow
+		} else {
+			board2 = append(board2, tempNodeRow)
+		}
+		linecount++
+	}
+	return &board2[startnodeY][startnodeX], &board2[endnodeY][endnodeX], board2
+}
+
+func calculateF(current *node, stop *node) int {
+	return current.g + int(math.Abs(float64(current.x-stop.x))+math.Abs(float64(current.y-stop.y)))
+}
+
+func nodeInSet(nod *node, set []*node) bool {
+	for _, elem := range set {
+		if nod.x == elem.x && nod.y == elem.y {
+			return true
+		}
+	}
+	return false
+}
+
+func findNeighbors(board [][]node, height int, width int, current *node) []*node {
+	neighbors := make([]*node, 0)
+	for i := max(0, current.x-1); i < min(width, current.x+2); i++ {
+		if current.x != i && !board[current.y][i].block {
+			neighbors = append(neighbors, &board[current.y][i])
+		}
+	}
+	for j := max(0, current.y-1); j < min(height, current.y+2); j++ {
+		if current.y != j && !board[j][current.x].block {
+			neighbors = append(neighbors, &board[j][current.x])
+		}
+	}
+	return neighbors
+}
+
+func printPath(board [][]node, stopnode *node, startnode *node) {
+	var current *node
+	current = stopnode
+	for current != startnode {
+		current.solution = true
+		fmt.Println("Y: ", current.y, "X: ", current.x)
+		current = current.parent
+
+	}
+	fmt.Println("Y: ", current.y, "X: ", current.x)
+}
+
+func aStarSolve(startnode *node, stopnode *node, board [][]node, height int, width int) bool {
+	height = len(board)
+	width = len(board[0])
+	open := make([]*node, 1)
+	closed := make([]*node, 0)
+	open[0] = startnode
+	open[0].g = 0 // startnode cost zero
+	open[0].f = calculateF(startnode, stopnode)
+	// Algorithm loop
+	for len(open) != 0 {
+		current := open[0]
+		fmt.Println("Current", *current)
+		if current.x == stopnode.x && current.y == stopnode.y { // done
+			return true
+		}
+		open = open[1:] // pops open
+		current.open = false
+		closed = append(closed, current)
+
+		neighbors := findNeighbors(board, height, width, current)
+		fmt.Println("Naboer :")
+		for _, node := range neighbors {
+			fmt.Println("Nabo: ", *node)
+		}
+		for _, node := range neighbors {
+			if nodeInSet(node, closed) {
+				tempg := current.g + 1
+				if tempg < node.g {
+					fmt.Println("Found a shorter path to a closed node, expand program") // don't think this will happen
+				}
+				continue
+			}
+			if !nodeInSet(node, open) {
+				fmt.Println("Legger til", *node)
+				open = append(open, node)
+				node.open = true
+			}
+			tempg := current.g + 1
+			if tempg >= node.g { // did not find abetter path
+				continue
+			}
+			// This path is best
+			node.parent = current
+			node.g = tempg
+			node.f = calculateF(node, stopnode)
+		}
+		sort.Sort(nodes(open))
+
+	}
+	return false
+}
+
+func drawImage(board [][]node, startnode *node, stopnode *node, filename string) {
+	// Creating squares of size 20x20 with borders
+	height := len(board)
+	width := len(board[0])
+	img := image.NewRGBA(image.Rect(0, 0, width*21+1, height*21+1))
+	for i := 0; i <= img.Bounds().Max.X; i += 21 {
+		for j := 0; j <= img.Bounds().Max.Y; j++ {
+			img.Set(i, j, color.RGBA{0, 0, 0, 255})
+		}
+	}
+	for j := 0; j <= img.Bounds().Max.Y; j += 21 {
+		for i := 0; i <= img.Bounds().Max.X; i++ {
+			img.Set(i, j, color.RGBA{0, 0, 0, 255})
+		}
+	}
+
+	img.Set(10, 19, color.RGBA{255, 0, 0, 255})
+
+	file, _ := os.Create(filename)
+	defer file.Close()
+	png.Encode(file, img)
+}
+
+func main() {
+	var file = os.Args[1]
+	fmt.Println(os.Args)
+	startnode, stopnode, board := readBoard(file)
+	/*
+		for _, row := range board {
+			for _, node := range row {
+				fmt.Print(node)
+			}
+			fmt.Println()
+		}
+	*/
+	fmt.Println(startnode)
+	fmt.Println(stopnode)
+	height := len(board)
+	width := len(board[1])
+	aStarSolve(startnode, stopnode, board, height, width)
+	printPath(board, stopnode, startnode)
+	imagename := strings.Replace(string(file), "txt", "png", -1)
+	fmt.Println(imagename)
+	drawImage(board, startnode, stopnode, imagename)
+}
